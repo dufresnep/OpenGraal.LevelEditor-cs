f60a52998a1247a384b036131020b6ff6168fa1b
Paul Dufresne <dufresnep@gmail.com>
2015-04-05 19:32:44 -0400

    Commented out LevelEdit.MainWindow (was partial class causing already def err)

---
+++ LevelEditor/gtk-gui/OpenGraal.LevelEditor.MainWindow.cs
@@ -0,0 +1,30 @@
+
+/*namespace OpenGraal.LevelEditor
+{
+	public partial class MainWindow
+	{
+		private Gtk.UIManager UIManager;
+		
+		private Gtk.VBox vbox1;
+		
+		private Gtk.MenuBar menubar1;
+		
+		private Gtk.HPaned hpaned1;
+		
+		private Gtk.ScrolledWindow scrolledwindow1;
+		
+		private Gtk.Image levelDrawPane;
+		
+		private Gtk.Notebook notebook1;
+		
+		private Gtk.ScrolledWindow scrolledwindow2;
+		
+		private Gtk.Image tilesetDrawPane;
+		
+		private Gtk.Label tilesetLbl;
+		
+		private Gtk.Label layersLbl;
+		
+		private Gtk.Label reportLbl;
+	}
+}*/
